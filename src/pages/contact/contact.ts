cc843928f48d3db7bde647dc8a1f0feb86d9867e
s64309010028 <s64309010028@ptnicec.ac.th>
2022-11-01 17:29:23 +0700

    ส่งครั้งที่1

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,8 +7,25 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+    // Doughnut
+public doughnutChartLabels:string[] = ['ปวส.1', 'ปวส.2', 'ปวส.3'];
+public doughnutChartData:number[] = [350, 450, 100];
+public doughnutChartType:string = 'doughnut';
+
+// events
+public chartClicked(e:any):void {
+  console.log(e);
+}
+
+public chartHovered(e:any):void {
+  console.log(e);
+}
+
+
+
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
